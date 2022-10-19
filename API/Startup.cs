0a3082e25db57ce1ef69fcbe8f56066679626eec
Dan Ryan <dan@Dans-MacBook-Pro.local>
2022-10-19 16:46:39 +0100

    shuubuus

--- API/Startup.cs
+++ API/Startup.cs
@@ -36,10 +36,12 @@ namespace API
                options.UseSqlite(_config.GetConnectionString("DefaultConnections"));
             });
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
             });
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,6 +58,8 @@ namespace API
 
             app.UseRouting();
 
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+            
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
